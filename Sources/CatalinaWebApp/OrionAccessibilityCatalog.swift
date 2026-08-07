a747bdfb834625a309c9f67c2ea3cf383f80be07
79cbd8hmgj-wq <lebrechte@icloud.com>
2026-08-07 18:33:00 -0400

    feat: freeze verified Orion accessibility catalog

---
+++ Sources/CatalinaWebApp/OrionAccessibilityCatalog.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct OrionAccessibilityCatalog {
+    let fileMenuTitle = "File"
+    let profilesMenuTitle = "Profiles"
+    let viewMenuTitle = "View"
+    let focusModeIdentifier = "FocusMode"
+    let enableFocusModeTitle = "Enable Focus Mode"
+    let disableFocusModeTitle = "Disable Focus Mode"
+    let dedicatedProfileName = "CatalinaWeb"
+    let supportsAutomaticProfileCreation = false
+}
