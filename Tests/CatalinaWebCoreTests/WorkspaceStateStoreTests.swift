1cfae4456551d010cb54d9ccfed7ba8dfb4b4ec2
79cbd8hmgj-wq <lebrechte@icloud.com>
2026-08-07 17:31:10 -0400

    test: cover Orion-backed state migration

--- Tests/CatalinaWebCoreTests/WorkspaceStateStoreTests.swift
+++ Tests/CatalinaWebCoreTests/WorkspaceStateStoreTests.swift
@@ -21,11 +21,21 @@ final class WorkspaceStateStoreTests: XCTestCase {
 
     func testExactStateRoundTrips() {
         let store = UserDefaultsWorkspaceStateStore(defaults: defaults)
+        let profile = OrionProfileIdentity(
+            applicationURL: URL(fileURLWithPath: "/Users/test/Applications/Orion/Orion Profiles/CatalinaWeb.app"),
+            bundleIdentifier: "com.kagi.kagimacOS.CatalinaWeb",
+            localizedName: "CatalinaWeb"
+        )
         let expected = PersistedWorkspaceState(
             lastChatGPTURL: URL(string: "https://chatgpt.com/c/round-trip"),
             lastGitHubURL: URL(string: "https://github.com/79cbd8hmgj-wq/CatalinaWeb/tree/main"),
             activeWorkspace: .github,
-            windowFrame: "{{10, 20}, {1100, 760}}"
+            controllerWindowFrame: "{{10, 20}, {520, 120}}",
+            orionWindowFrame: "{{40, 50}, {1100, 760}}",
+            lastChatGPTActivatedAt: Date(timeIntervalSince1970: 100),
+            lastGitHubActivatedAt: Date(timeIntervalSince1970: 200),
+            setupStage: .profileVerified,
+            orionProfileIdentity: profile
         )
 
         store.save(expected)
@@ -33,7 +43,39 @@ final class WorkspaceStateStoreTests: XCTestCase {
         XCTAssertEqual(store.load(), expected)
     }
 
-    func testCorruptDataReturnsInitialWithoutRewritingInvalidPayload() {
+    func testV1StateMigratesWindowFrameToControllerFrame() throws {
+        let legacy = """
+        {"activeWorkspace":"github","windowFrame":"{{10, 20}, {900, 700}}"}
+        """.data(using: .utf8)!
+        defaults.set(legacy, forKey: UserDefaultsWorkspaceStateStore.legacyStorageKey)
+
+        let store = UserDefaultsWorkspaceStateStore(defaults: defaults)
+        let state = store.load()
+
+        XCTAssertEqual(state.activeWorkspace, .github)
+        XCTAssertEqual(state.controllerWindowFrame, "{{10, 20}, {900, 700}}")
+        XCTAssertNil(state.orionWindowFrame)
+        XCTAssertEqual(state.setupStage, .notStarted)
+        XCTAssertNotNil(defaults.data(forKey: UserDefaultsWorkspaceStateStore.storageKey))
+        XCTAssertEqual(defaults.data(forKey: UserDefaultsWorkspaceStateStore.legacyStorageKey), legacy)
+    }
+
+    func testV2StateWinsWhenLegacyStateAlsoExists() throws {
+        let store = UserDefaultsWorkspaceStateStore(defaults: defaults)
+        var current = PersistedWorkspaceState.initial
+        current.activeWorkspace = .chatGPT
+        current.controllerWindowFrame = "{{1, 2}, {520, 120}}"
+        store.save(current)
+
+        let legacy = """
+        {"activeWorkspace":"github","windowFrame":"{{10, 20}, {900, 700}}"}
+        """.data(using: .utf8)!
+        defaults.set(legacy, forKey: UserDefaultsWorkspaceStateStore.legacyStorageKey)
+
+        XCTAssertEqual(store.load(), current)
+    }
+
+    func testCorruptV2DataReturnsInitialWithoutRewritingInvalidPayload() {
         let store = UserDefaultsWorkspaceStateStore(defaults: defaults)
         let corrupt = Data([0x00, 0x01, 0x02, 0x03])
         defaults.set(corrupt, forKey: UserDefaultsWorkspaceStateStore.storageKey)
